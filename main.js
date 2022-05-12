df826892e1d57383b8f578c4748f6cfa244c524b
Santasri97 <90270303+Santasri97@users.noreply.github.com>
2022-05-12 19:57:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+var noseX = 0;
+var noseY = 0;
+
+function preload() {
+    clownnose = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
+}
+ function setup() {
+     canvas = createCanvas(300, 300);
+     canvas.center();
+     video = createCapture(VIDEO);
+     video.size(300,300);
+     video.hide();
+     poseNet = ml5.poseNet(video,modelloaded);
+     poseNet.on('pose', gotPoses);
+ }
+
+ function modelloaded(){
+     console.log("poseNet is initialized");
+ }
+
+ function draw() {
+     image(video,0,0,300,300);
+     image(clownnose,noseX,noseY,60,30);
+ }
+
+ function button() {
+     save("my_filtered_image.png");
+ }
+
+ function gotPoses(results) {
+     if (results.length > 0)
+     {
+         console.log(results);
+         noseX = results[0].pose.nose.x -30;
+         noseY = results[0].pose.nose.y +7;
+         console.log("nose x = " + noseX);
+         console.log("nose y = " + noseY);
+     }
+ }
